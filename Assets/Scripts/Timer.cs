89f38b7df21f486c04053786ac1cad4371927a71
ElfMerchant <danil.gavshin@gmail.com>
2021-11-04 22:04:53 +0300

    StartJam

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    Image timerBar;
+    public float maTime = 20f;
+    float timeLeft;
+    void Start()
+    {
+        timerBar = GetComponent<Image>();
+        timeLeft = maTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            timerBar.fillAmount = timeLeft / maTime;
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
