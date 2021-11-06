ed57f17b5d2053aa5b24e64f539aec11bedb4510
SegoeScript <segoescrypt@gmail.com>
2021-11-06 21:37:48 +0300

    screen settings

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -2,13 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    Resolution[] resolutions;
+    public Dropdown resolutionDropdown;
+
+    private void Start()
+    {
+        int CurrentResolutionIndex = 0;
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string Option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(Option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                CurrentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = CurrentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
 
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("Volume", volume);
     }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }    
 }
