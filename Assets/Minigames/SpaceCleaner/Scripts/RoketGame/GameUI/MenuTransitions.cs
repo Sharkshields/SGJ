3f20b8c8cc31e05be095ef96cfc3f2d90b31b9d0
Sali <43134249+salidartilas2001gate@users.noreply.github.com>
2021-11-07 12:37:00 +0300

    add full version minigame

--- Assets/Minigames/SpaceCleaner/Scripts/RoketGame/GameUI/MenuTransitions.cs
+++ Assets/Minigames/SpaceCleaner/Scripts/RoketGame/GameUI/MenuTransitions.cs
@@ -7,10 +7,10 @@ public class MenuTransitions : MonoBehaviour
 {
     public void onClickExit()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene("SpaceCleanerMainMenu");
     }
     public void onClickRestart()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("SpaceCleanerGame");
     }
 }
